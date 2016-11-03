1efbb765bc0914c5a5f82dc5c15181565b4c4bcb
Gregory Bilello & Melanie Gershman <pair+gvbilello.melaniegershman@devbootcamp.com>
2016-11-03 18:13:46 -0400

    Configure nav bar layout, line height, text and list deco using CSS

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -1,10 +1,22 @@
 get '/questions' do
-  @questions = Question.all
+  @questions = Question.all.reverse
   erb :index
 end
 
 post '/questions' do
-  erb :'/questions/new'
+  # binding.pry
+  @question = Question.new(title: params[:title], description: params[:description], author: User.find_by(id: session["user_id"]))
+  if @question.save
+    # do good things
+    status 200
+    # go smwhr nice
+    # binding.pry
+    erb :'/question/_question', locals: {question: @question}, layout: false
+  else
+    # error messages!!!
+    status 404
+    erb :index
+  end
 end
 
 get '/questions/:question_id' do
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,24 @@
 $(document).ready(function () {
+  $(".new-question").click(function() {
+    $(this).addClass("hidden");
+    $("#new-question").removeClass("hidden");
+  });
+
+  $("#new-question").submit(function(event) {
+    event.preventDefault();
+
+    var ajaxRequest = $.ajax({
+      url: '/questions',
+      method: 'POST',
+      data: $(this).serialize()
+    });
+
+    ajaxRequest.done(function(response) {
+      $("ul.questions").prepend(response);
+      $("form#new-question")[0].reset();
+      $(".new-question").removeClass("hidden");
+      $("form#new-question").addClass("hidden");
+    });
+  });
 
 });
