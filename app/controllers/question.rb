c0ed8631f446fee942dab6a4557e0f6c38ff5803
Gregory Bilello & Melanie Gershman <pair+gvbilello.melaniegershman@devbootcamp.com>
2016-11-03 14:51:09 -0400

    Working on CRUD

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -9,6 +9,7 @@ end
 
 get '/questions/:question_id' do
   @question = Question.find_by(id: params[:question_id])
-
+  @answers = Answer.find_by(question_id: params[:question_id])
+  binding.pry
   erb :'/question/show'
 end
