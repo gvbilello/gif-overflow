151edfa01499097a37dee3279bfec84178fe2631
Melanie Gershman & Sofia Cepeda <pair+melaniegershman.sofiaclara93@devbootcamp.com>
2016-11-04 15:08:26 -0400

    creeate appended comment

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,5 +1,15 @@
 post '/questions/:question_id/comments' do
+  @question = Question.find_by(id: params[:question_id])
+  # binding.pry
+  if request.xhr?
+    @comment = Comment.new(response: params[:comment_text], commentable_id: params[:question_id], commentable_type: "Question", commenter: current_user)
+    if @comment.save
+      erb :'/question/_questioncommentform', layout: false
+    end
+  end
+
 end
 
 post '/answers/:answer_id/comments' do
 end
+
--- public/js/application.js
+++ public/js/application.js
@@ -36,7 +36,6 @@ $(document).ready(function () {
 
     $('li').on('click', '.delete', function(event){
     event.preventDefault();
-    var thingy = event.target
     var route = $(this).attr("href");
     $.ajax({
       url: route,
@@ -44,6 +43,21 @@ $(document).ready(function () {
     }).done(function(response) {
       // Track down question to be deleted by its ID and use that as a class to query and remove
       $('.' + response.id).remove()
+    });
+  });
+
+    $('form#new-comment').on('submit', function(event){
+    event.preventDefault();
+    var route = $(this).attr("action");
+    var data = $(this).serialize();
+    $.ajax({
+      url: route,
+      method: "post",
+      data: data
+    }).done(function(serverResponse) {
+      console.log("I'm the server response", serverResponse)
+      $(".comments").append(serverResponse);
+      $("form#new-comment").find("input[name='comment_text']").val('')
     });
   });
 
