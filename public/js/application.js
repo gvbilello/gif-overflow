33f185a0124ee5058e22ad5fa19ea33130d33c7a
Scott Dixon <dixon.scottr@gmail.com>
2016-11-04 08:00:21 -0400

    Fix JS issue to let users post questions and answers

--- public/js/application.js
+++ public/js/application.js
@@ -7,32 +7,30 @@ $(document).ready(function () {
 
   $("#new-question").submit(function(event) {
     event.preventDefault();
-
     var ajaxRequest = $.ajax({
       url: '/questions',
       method: 'POST',
       data: $(this).serialize()
     });
-
     ajaxRequest.done(function(response) {
       $("ul.questions").prepend(response);
       $("form#new-question")[0].reset();
       $(".new-question").removeClass("hidden");
       $("form#new-question").addClass("hidden");
+    });
+  });
 
   $('#answer-form').on('submit', function(event){
     event.preventDefault();
     var route = $(this).attr("action");
     var answerData = $(this).find("textarea").serialize();
-    // debugger;
     $.ajax({
       url: route,
       method: "post",
       data: answerData
     }).done(function(serverResponse) {
-      // debugger;
       $(".answer-box").find(".answers").append(serverResponse)
-
+      $('#answer-form').find('textarea').val('')
     });
   });
 
