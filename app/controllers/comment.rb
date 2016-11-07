073c84aae5452c8f66a32bc23039abafb8330869
Scott Dixon <dixon.scottr@gmail.com>
2016-11-07 07:44:42 -0500

    Remove add comment/answer form from logged out users

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,6 +1,5 @@
 post '/questions/:question_id/comments' do
   @question = Question.find_by(id: params[:question_id])
-  # binding.pry
   if request.xhr?
     @comment = Comment.new(response: params[:comment_text], commentable_id: params[:question_id], commentable_type: "Question", commenter: current_user)
     if @comment.save
